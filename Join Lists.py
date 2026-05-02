fe980a97044f6b8ebbab5416550ba0860794e3f2
Shila Hosseinzadeh <101890822+Shila-Hosseinzadeh@users.noreply.github.com>
2026-05-02 13:48:56 +0330

    Update Join Lists.py

--- Join Lists.py
+++ Join Lists.py
@@ -38,3 +38,15 @@ list1.extend(list2)
 print(f"list1.extend(list2) : {list1}")
 
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+#join with *
+
+list1 = [ 1,2,3,4,5]
+list2 = [11,12,13,14,15]
+print(f"list1 : {list1}")
+print(f"list2 : {list2}")
+
+
+print([x for x in [*list1 , *list2]]) #print(f"list2 +list2 : {[*list1 , *list2]}")
+
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
